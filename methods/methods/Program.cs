582df03a15607a43e48f6f0fad312fb497e0d1d4
indianajim <jimrunkel@gmail.com>
2019-03-31 18:51:55 -0400

    repeat methods practice

---
+++ methods/methods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Addition of 15 and 31 = " + Add(15, 31));
+            Console.WriteLine("Multiplication of 15 and 31 = " + Mult(15, 31));
+            Console.Read();
+
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Mult(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
