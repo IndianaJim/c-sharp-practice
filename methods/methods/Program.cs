dee16f834a9acac4d910a76c5765f9d9917afb10
indianajim <jimrunkel@gmail.com>
2019-04-01 21:43:58 -0400

    more methods

--- methods/methods/Program.cs
+++ methods/methods/Program.cs
@@ -8,12 +8,29 @@ namespace methods
 {
     class Program
     {
+       
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Addition of 15 and 31 = " + Add(15, 31));
-            Console.WriteLine("Multiplication of 15 and 31 = " + Mult(15, 31));
+            string friend1 = "Brad";
+            string friend2 = "Doug";
+            string friend3 = "Phil";
+
+            GreetFriend(friend1);
+            GreetFriend(friend2);
+            GreetFriend(friend3);
             Console.Read();
 
+            //Console.WriteLine("Addition of 15 and 31 = " + Add(15, 31));
+            //Console.WriteLine("Multiplication of 15 and 31 = " + Mult(15, 31));
+            //Console.Read();
+
+        }
+
+        public static void GreetFriend(string name)
+        {
+            Console.WriteLine("Hi " + name + ", my friend");
+            
         }
 
         public static int Add(int num1, int num2)
