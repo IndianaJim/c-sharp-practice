8a1210bfae746cba1bbf89d7e1767bdfc118dd1c
indianajim <jimrunkel@gmail.com>
2019-04-03 21:49:34 -0400

    initial decisions samples

---
+++ decision-statements/decision-statements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace decision_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter temp: ");
+            var temperature = Console.ReadLine();
+            
+            if(int.TryParse(temperature,out int temperatureNumber))
+            {
+                if(temperatureNumber < 40)
+                {
+                    Console.WriteLine("Take a coat!");
+                }else if (temperatureNumber == 40)
+                {
+                    Console.WriteLine("It's 40 degress out!");
+                }else
+                {
+                    Console.WriteLine("Warm out!");
+                }
+            }else Console.WriteLine("That's not a number!");
+
+            Console.Read();
+        }
+    }
+}
