eb59d41cbbc2cc5500596d20a64cbfd7f6113823
indianajim <jimrunkel@gmail.com>
2019-04-07 09:13:41 -0400

    more if

--- decision-statements/decision-statements/Program.cs
+++ decision-statements/decision-statements/Program.cs
@@ -10,26 +10,56 @@ namespace decision_statements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter temp: ");
-            var temperature = Console.ReadLine();
+            //checkTemp();
+            checkRegistered();
             
-            if(int.TryParse(temperature,out int temperatureNumber))
-            {
-                if(temperatureNumber < 40)
-                {
-                    Console.WriteLine("Take a coat!");
-                }else if (temperatureNumber == 40)
-                {
-                    Console.WriteLine("It's 40 degress out!");
-                }else
-                {
-                    Console.WriteLine("Warm out!");
-                }
-            }else Console.WriteLine("That's not a number!");
+        }
 
+        static void checkRegistered()
+        {
+            bool isAdmin = false;
+            bool isRegistered = true;
+            string userName = "";
+            Console.WriteLine("Please enter your name: ");
+            userName = Console.ReadLine();
+            if (isRegistered)
+            {
+                Console.WriteLine("Hi there, registered user " );
+                if(userName != "")
+                {
+                    Console.WriteLine("Hi " + userName);
+                    if(userName.Equals("Admin"))
+                    {
+                        Console.WriteLine("Hi Admin");
+                    }
+                }
+            }
             Console.Read();
         }
 
-        static void checkRegistered
+        static void checkTemp()
+        {
+            Console.WriteLine("Enter temp: ");
+            var temperature = Console.ReadLine();
+
+            if (int.TryParse(temperature, out int temperatureNumber))
+            {
+                if (temperatureNumber < 40)
+                {
+                    Console.WriteLine("Take a coat!");
+                }
+                else if (temperatureNumber == 40)
+                {
+                    Console.WriteLine("It's 40 degress out!");
+                }
+                else
+                {
+                    Console.WriteLine("Warm out!");
+                }
+            }
+            else Console.WriteLine("That's not a number!");
+
+            Console.Read();
+        }
     }
 }
